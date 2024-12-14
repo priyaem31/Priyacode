40b38a01464fcec707e97fe282bf289ec73d875d
priyaem31 <83340896+priyaem31@users.noreply.github.com>
2024-12-14 22:11:08 +0530

    Add files via upload

---
+++ set1prblm1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 System.out.println("Enter values : ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String b = scan.next();
+        char c = scan.next().charAt(0);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+	}
+}
